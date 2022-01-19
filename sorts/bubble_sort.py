4d959468d5c825d3eea318d6a4884ef20d427753
Conor <conor@johngregg.org>
2022-01-19 17:12:44 -0600

    Added Bubble Sort

---
+++ sorts/bubble_sort.py
@@ -0,0 +1,50 @@
+import random
+import time
+
+
+def sort(lst):
+    """
+    2.1533379399974364 for 5000 elements
+    """
+    for i in range(len(lst)):
+        for j in range(len(lst) - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j + 1], lst[j] = lst[j], lst[j + 1]
+    return lst
+
+
+def sort1(lst):
+    """
+    1.37956524999463 for 5000 elements
+    """
+    for i in range(len(lst)):
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j + 1], lst[j] = lst[j], lst[j + 1]
+    return lst
+
+
+def sort2(lst):
+    """
+    1.3715944900002797 for 5000 elements
+    """
+    for i in range(len(lst)):
+        swapped = False
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j + 1], lst[j] = lst[j], lst[j + 1]
+                swapped = True
+        if not swapped:
+            return lst
+    return lst
+
+
+if __name__ == '__main__':
+    repeat = 10
+    s = 0
+    for _ in range(repeat):
+        arr = [random.randint(1, 1000) for _ in range(5000)]
+        start = time.perf_counter()
+        sort2(arr)
+        s += time.perf_counter() - start
+    print(s / repeat)
