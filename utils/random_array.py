e88e5a3c69fa48f31a651e07ac08aecd011346a3
Conor <conor@johngregg.org>
2022-01-20 13:46:39 -0600

    Added type hints

--- utils/random_array.py
+++ utils/random_array.py
@@ -1,5 +1,5 @@
 import random
 
 
-def random_array(a, b, length):
-    return [random.randint(a, b) for _ in range(length)]
+def random_array(low: int, high: int, length: int) -> list[int]:
+    return [random.randint(low, high) for _ in range(length)]
